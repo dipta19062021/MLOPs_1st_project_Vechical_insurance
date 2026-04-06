f5872ef22e3c799f5da2a96272ca7917e259d9d8
Archisman Ghosh <ghosharchisman58@gmail.com>
2026-04-06 10:14:01 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Archisman Ghosh",
+    author_email="ghosharchisman58@gmail.com",
+    packages=find_packages()
+)
